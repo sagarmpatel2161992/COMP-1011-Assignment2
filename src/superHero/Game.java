2592c151beb116e89afca58820cb11a4d36ef7c9
sagarmpatel2161992 <sagarmpatel2161992@yahoo.com>
2015-02-06 19:00:07 -0800

    SuperHero Class Added

---
+++ src/superHero/Game.java
@@ -0,0 +1,38 @@
+package superHero;
+/**
+ * 
+ */
+
+/**
+ * @author SAGAR
+ * Program Description - Main Method for Hero Class
+ * Version: 1.0 29th january 2015
+ *  
+ *
+ */
+public class Game {
+
+	/**
+	 * 
+	 */
+	public Game() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Hero sagar = new Hero("Saggy");
+		sagar.show();
+		sagar.fight();
+		sagar.fight();
+		sagar.fight();
+		
+
+
+	}
+
+}
---
+++ src/superHero/SuperHero.java
@@ -0,0 +1,10 @@
+package superHero;
+
+public class SuperHero extends Hero {
+
+	public SuperHero(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
