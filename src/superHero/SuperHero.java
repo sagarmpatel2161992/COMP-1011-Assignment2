83b8238cea1278fc251a0293e7a312ee085f3051
sagarmpatel2161992 <sagarmpatel2161992@yahoo.com>
2015-02-06 19:03:47 -0800

    generateRendomPowers and showPowers method added

--- src/superHero/SuperHero.java
+++ src/superHero/SuperHero.java
@@ -1,10 +1,22 @@
 package superHero;
 
 public class SuperHero extends Hero {
+	
+	private String[] superPowers = new String[3];
+	 private String[] powers ={"Super Speed","Super Strength", "Body Armour","Flight","Fire Generation","Weather Control"};
 
-	public SuperHero(String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
-	}
-
+	 public SuperHero(String name) {
+			super(name);
+			generateRandomPowers();
+			// TODO Auto-generated constructor stub
+		}
+		private void generateRandomPowers()
+		{	
+			
+		}
+		
+		public void showPowers()
+		{
+		
+		}
 }
